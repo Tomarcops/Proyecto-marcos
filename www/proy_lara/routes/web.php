208e8b6ebf607c94272670d7ede86e3d01fe022e
marcos david torres diaz <torresdiaz685@gmail.com>
2020-12-17 15:43:29 -0600

    commit inicial

--- www/proy_lara/routes/web.php
+++ www/proy_lara/routes/web.php
@@ -1,33 +1,33 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
-
 use App\Http\Controllers\CasillaController;
-use App\Http\Controllers\CandidatoController;
 use App\Http\Controllers\FuncionarioController;
-use App\Http\Controllers\EleccioncomiteController;
+use App\Http\Controllers\CandidatoController;
 use App\Http\Controllers\RolController;
 use App\Http\Controllers\EleccionController;
 use App\Http\Controllers\VotoController;
-use App\Http\Controllers\VotocandidatoController;
+use App\Http\Controllers\EleccioncomiteController;
 use App\Http\Controllers\FuncionariocasillaController;
 use App\Http\Controllers\ImeiautorizadoController;
-
-
-
+use App\Http\Controllers\VotocandidatoController;
 
 
 Route::get('/', function () {
     return view('welcome');
+
+
 });
+
 Route::resource('casilla', CasillaController::class);
-Route::resource('candidato', CandidatoController::class);
 Route::resource('funcionario', FuncionarioController::class);
-Route::resource('eleccioncomite', EleccioncomiteController::class);
-Route::resource('eleccion', EleccionController::class);
-Route::resource('rol', RolController::class);
-Route::resource('voto', VotoController::class);
-Route::resource('votocandidato', VotocandidatoController::class);
-Route::resource('funcionariocasilla', FuncionariocasillaController::class);
-Route::resource('imeiautorizado', ImeiautorizadoController::class);
+Route::resource('candidato', CandidatoController::class);
+Route::resource('rol',RolController::class);
+Route::resource('eleccion',EleccionController::class);
+Route::resource('voto',VotoController::class);
+Route::resource('funcionariocasilla',FuncionariocasillaController::class);
+Route::resource('eleccioncomite',EleccioncomiteController::class);
+Route::resource('imeiautorizado',ImeiautorizadoController::class);
+
+
+Route::resource('votocandidato',VotocandidatoController::class);
